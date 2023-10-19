9a2238b2c7329c5ea7da5697696f34f757bed0aa
LeonBerry07 <altairconnor12@gmail.com>
2023-10-19 15:54:36 -0300

    bubbles.effect

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -75,8 +75,53 @@ class LandingPage extends React.Component{
 
             </header>
             
-            {/* <div class="wave"></div> */}
-
+            <div class="container">
+                <div class='bubbles'>
+                    <span style={{ '--i' : 23 }}></span>
+                    <span style={{ '--i' : 12 }}></span>
+                    <span style={{ '--i' : 17 }}></span>
+                    <span style={{ '--i' : 29 }}></span>
+                    <span style={{ '--i' : 23 }}></span>
+                    <span style={{ '--i' : 24 }}></span>
+                    <span style={{ '--i' : 25 }}></span>
+                    <span style={{ '--i' : 21 }}></span>
+                    <span style={{ '--i' : 14 }}></span>
+                    <span style={{ '--i' : 29 }}></span>
+                    <span style={{ '--i' : 18 }}></span>
+                    <span style={{ '--i' : 27 }}></span>
+                    <span style={{ '--i' : 11 }}></span>
+                    <span style={{ '--i' : 21 }}></span>
+                    <span style={{ '--i' : 15 }}></span>
+                    <span style={{ '--i' : 27 }}></span>
+                    <span style={{ '--i' : 13 }}></span>
+                    <span style={{ '--i' : 23 }}></span>
+                    <span style={{ '--i' : 27 }}></span>
+                    <span style={{ '--i' : 17 }}></span>
+                    <span style={{ '--i' : 15 }}></span>
+                    <span style={{ '--i' : 22 }}></span>
+                    <span style={{ '--i' : 20 }}></span>
+                    <span style={{ '--i' : 17 }}></span>
+                    <span style={{ '--i' : 19 }}></span>
+                    <span style={{ '--i' : 14 }}></span>
+                    <span style={{ '--i' : 21 }}></span>
+                    <span style={{ '--i' : 12 }}></span>
+                    <span style={{ '--i' : 27 }}></span>
+                    <span style={{ '--i' : 14 }}></span>
+                    <span style={{ '--i' : 21 }}></span>
+                    <span style={{ '--i' : 13 }}></span>
+                    <span style={{ '--i' : 11 }}></span>
+                    <span style={{ '--i' : 29 }}></span>
+                    <span style={{ '--i' : 17 }}></span>
+                    <span style={{ '--i' : 16 }}></span>
+                    <span style={{ '--i' : 20 }}></span>
+                    <span style={{ '--i' : 30 }}></span>
+                    <span style={{ '--i' : 18 }}></span>
+                    <span style={{ '--i' : 19 }}></span>
+                    <span style={{ '--i' : 21 }}></span>
+                    <span style={{ '--i' : 13 }}></span>
+                    <span style={{ '--i' : 25 }}></span>
+                    <span style={{ '--i' : 17 }}></span>
+                </div>
 
 
             <div className="tim">
@@ -275,7 +320,7 @@ class LandingPage extends React.Component{
             </div>
 
 
-
+            </div>
 
 
 
